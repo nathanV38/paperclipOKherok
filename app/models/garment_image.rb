faae561202e32683b3db1922c5518a1c7f2a9c13
ilyes mathieu <ilyes.mathieu@gmail.com>
2013-05-24 16:58:37 +0200

    heroku2

--- app/models/garment_image.rb
+++ app/models/garment_image.rb
@@ -2,7 +2,7 @@ class GarmentImage < ActiveRecord::Base
   attr_accessible :, :, :caption, :garment_id
   
   belongs_to :garment
-has_attached_file :photo,  :styles => { :small => “150×150>”, :large => “320×240>” }
+has_attached_file :photo,  :styles => { :small => "150×150>", :large => "320×240>" }
 validates_attachment_presence : photo
 validates_attachment_size : photo, :less_than => 1.megabytes
 end
