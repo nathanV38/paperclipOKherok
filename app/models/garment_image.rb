6321c789b9ae36c161190ce2dd621da4aebf7add
ilyes mathieu <ilyes.mathieu@gmail.com>
2013-05-24 17:21:34 +0200

    heroku6

--- app/models/garment_image.rb
+++ app/models/garment_image.rb
@@ -6,6 +6,6 @@ has_attached_file :photo,  styles: {
 		:small => '150x150#', 
 		:large => '320x240#' 
 	}
-validates_attachment_presence : photo
-validates_attachment_size : photo #, :less_than => 1.megabytes
+validates_attachment_presence :photo
+validates_attachment_size :photo , :less_than => 1.megabytes
 end
